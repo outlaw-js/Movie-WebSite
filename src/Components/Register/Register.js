17b1aa539366ce0ac4719c8035c8216fb17f7e20
pouriya-chibaie-dev <85185561+pouriya-chibaie-dev@users.noreply.github.com>
2022-08-19 14:08:43 +0430

    validate form register

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -4,9 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import { Formik ,useFormik} from "formik";
 
-// import "./font/font.css";
-// import Header from "./Header";
 import {
   LoginBox,
   buttonFormStyle,
@@ -17,6 +16,7 @@ import {
   pushToLogOrReg,
   divContainerFlex,
 } from "../Login/LoginStyle";
+
 const Register = () => {
   const [showPassword, setShowPassword] = useState(true);
 
@@ -24,12 +24,40 @@ const Register = () => {
   const pushToLogin = () => {
     navigate(`/login`);
   };
-  const submitForm = () => {
   
-    const body1 = {
-      email: emailUser,
-      fullname: username,
-      password: passwordUser,
+  return (
+    <>
+        <div style={divContainer}>
+        <div style={LoginBox}>
+          <h1 style={titleForm}>Register</h1>
+          <Formik 
+       initialValues={{ email: '', password: '',username:'' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         }
+         if (values.password=="") {
+           errors.password = 'Required';
+         }
+         if (values.password.length <6 && values.password.length!==0) {
+           errors.password = 'The length is less than 5';
+         }
+         if (!values.username) {
+           errors.username = 'Required';
+         }
+          else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+        const body1 = {
+      email: values.email,
+      fullname: values.username,
+      password: values.password,
     };
     const bodyJson = JSON.stringify(body1);
     axios
@@ -55,6 +83,7 @@ const Register = () => {
               navigate(`/login`);
             }, 4000);
         }
+        setSubmitting(true)
        
       })
       .catch((err) => {
@@ -69,44 +98,66 @@ const Register = () => {
           progress: undefined,
           
           });
-      });
-  };
+          setSubmitting(false)      });
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit} style={{width:"100%" ,display:"flex",alignItems:"center",flexDirection:"column"}}>
+         <div style={{position:"relative",width:"100%",display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center"}}>
+         
+         <div style={{ width: "80%" }}>
 
-  const [emailUser, setEmailUser] = useState("");
-  const [passwordUser, setPasswordUser] = useState("");
-  const [username, setUsername] = useState("");
-  return (
-    <>
-        <div style={divContainer}>
-        <div style={LoginBox}>
-          <h1 style={titleForm}>Register</h1>
-          <div style={{ marginTop:"20px", width: "80%" }}>
-            <input
-              placeholder="Email"
-              style={inputStyle}
-              onChange={(e) => setEmailUser(e.target.value)}
-            ></input>
-          </div>
-          <div style={{ marginTop:"20px",background: "#E7E7E7",width: "80%",display:"flex",alignItems:"center",height:"55px" }}>
-          <VisibilityIcon onClick={()=>setShowPassword(!showPassword)}/>
-     
-            <input 
-            type={showPassword==false ?'password' :'text'}
-              placeholder="Password"
-              onChange={(e) => {
-                setPasswordUser(e.target.value);
-              }}
-              style={inputStyle}
-            />
+           <input style={inputStyle}
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+             placeholder="email"
+           />
+           {<span style={{color:"red"}}>{errors.email && touched.email && errors.email}</span>}
+         </div>
+         <div style={{ margin:"20px 0px 20px 0px" ,background: "#E7E7E7",width: "80%",display:"flex",alignItems:"center",height:"55px" }}>
+         <VisibilityIcon onClick={()=>setShowPassword(!showPassword)}/>
+           <input style={inputStyle} 
+              type={showPassword==false ?'password' :'text'}
+             name="password"
+             placeholder="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+           />
+         </div>
+           {<span style={{color:"red" , textAlign: "left",width:"80%"}}>{errors.password && touched.password && errors.password}</span>}
+           <div style={{ width: "80%" ,marginBottom:"20px"}}>
+
+           <input style={inputStyle}
+             type="text"
+             name="username"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.username}
+             placeholder="username"
+           />
+           {<span style={{color:"red"}}>{errors.username && touched.username && errors.username}</span>}
+         </div>
           
-          </div>
-          <div style={{  marginTop:"20px",width: "80%" }}>
-            <input
-              placeholder="name"
-              style={inputStyle}
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
-          </div>
+         </div>
+           <button type="submit"  style={buttonFormStyle} disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
           <div style={checkDivStyle}>
             <input
               type="checkbox"
@@ -115,9 +166,7 @@ const Register = () => {
             />
             <label>Remember me</label>
           </div>
-          <button style={buttonFormStyle} onClick={submitForm}>
-            REGISTER
-          </button>
+        
           <Link to={`/login`} style={pushToLogOrReg}>
             if you member login now
           </Link>
@@ -141,6 +190,6 @@ theme="colored"
 
     </>
   );
-};
+}
 
 export default Register;
